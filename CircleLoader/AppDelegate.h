dc5001723fc72e9624364d0c7b9bba862f7e4fa4
wuxueying <rainlesviowuxy@gamil.com>
2015-01-07 09:22:18 +0800

    Initial Commit

---
+++ CircleLoader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleLoader
+//
+//  Created by xueying on 15/1/7.
+//  Copyright (c) 2015年 wuxueying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleLoader
+//
+//  Created by xueying on 15/1/7.
+//  Copyright (c) 2015年 wuxueying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
